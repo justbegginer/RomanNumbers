3bf2ceb82b83623f646fb1730d215fffedff208a
justbegginer <blackcloudsinthesky@protonmail.com>
2020-01-02 11:21:19 +0300

    add simple convert to roman numbers

--- RomanNumber.py
+++ RomanNumber.py
@@ -9,9 +9,28 @@ class RomanNumber:
     def __convert_number(self):  # convert number into roman number
         roman = ""
         number = self.number
-        while number > 1000:
+        while number >= 1000:
             number -= 1000
             roman += "M"
+        while number >= 500:
+            number -= 500
+            roman += "D"
+        while number >= 100:
+            number -= 100
+            roman += "C"
+        while number >= 50:
+            number -= 50
+            roman += "L"
+        while number >= 10:
+            number -= 10
+            roman += "X"
+        while number >= 5:
+            number -= 5
+            roman += "V"
+        while number >= 1:
+            number -= 1
+            roman += "I"
+        self._roman_number = roman
 
     def __add__(self, other):
         self.number += other.number
