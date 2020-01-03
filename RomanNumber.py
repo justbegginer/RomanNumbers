3b478dbadd50959aee74e4f04788b701877230a0
justbegginer <blackcloudsinthesky@protonmail.com>
2020-01-03 12:18:21 +0300

    add assert check rebuild all math-operation functions because they are wrong and changed first object and return it

--- RomanNumber.py
+++ RomanNumber.py
@@ -33,35 +33,67 @@ class RomanNumber:
         self._roman_number = roman
 
     def __add__(self, other):
-        self.number += other.number
-        self.__convert_number()
-        return self
+        if type(other) is str:
+            return self._roman_number + other
+        if type(self) is str:
+            print("return")
+            return self + other._roman_number
+        new_roman = RomanNumber(self.number)
+        new_roman.number += other.number
+        new_roman.__convert_number()
+        return new_roman
 
     def __sub__(self, other):
-        self.number -= other.number
-        self.__convert_number()
-        return self
+        new_roman = RomanNumber(self.number)
+        new_roman.number -= other.number
+        new_roman.__convert_number()
+        return new_roman
 
     def __mul__(self, other):
-        self.number *= other.number
-        self.__convert_number()
-        return self
+        new_roman = RomanNumber(self.number)
+        new_roman.number *= other.number
+        new_roman.__convert_number()
+        return new_roman
 
     def __truediv__(self, other):
-        self.number //= other.number
-        self.__convert_number()
-        return self
+        new_roman = RomanNumber(self.number)
+        new_roman.number //= other.number
+        new_roman.__convert_number()
+        return new_roman
 
     def __floordiv__(self, other):
-        return self.__truediv__(self, other)
+        new_roman = self.__truediv__(other)
+        return new_roman
 
     def __mod__(self, other):
-        self.number %= other.number
-        self.__convert_number()
-        return self
+        new_roman = RomanNumber(self.number)
+        new_roman.number %= other.number
+        new_roman.__convert_number()
+        return new_roman
 
     def __eq__(self, other):
-        return self == other
+        if type(other) == str:
+            return self._roman_number == other
+        else:
+            return self.number == other.number
 
     def __repr__(self):
-        return self._roman_number
+        return str(self._roman_number)
+
+
+first_number = RomanNumber(2)
+second_number = RomanNumber(4)
+
+# assert type(first_number + second_number) is str, "no the type is " + str(type(first_number + second_number))
+
+# print(first_number + "F")
+
+# input()
+#print(second_number // first_number)
+
+assert str(first_number + second_number) == "VI", "should be six we've got "  # wtf
+assert str(second_number - first_number) == "II", "should be two"
+assert str(second_number / first_number) == "II" and str(
+    second_number // first_number) == "II", "in both case we should get two"
+assert str(second_number * first_number) == "VIII", "should be eight"
+assert second_number != first_number, "numbers not equal"
